78dd42131caaf3b7f648a5b2910afad68519d1c8
태관 <98436988+tkyoun0421@users.noreply.github.com>
2024-02-15 19:55:10 +0900

    Add: 프로젝트 초기 세팅 완료 (#1)
    * Chore: test
    
    * Add: alias 설정 완료
    
    * Add: 프로젝트 초기 라이브러리 설치 완료

--- craco.config.ts
+++ craco.config.ts
@@ -7,6 +7,7 @@ module.exports = {
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
     },
   },
 };
