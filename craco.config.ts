bc4020cfba4a6f69b45c47c48cf16de02c3b50a2
태관 <tkyoun0421@naver.com>
2024-02-15 19:06:08 +0900

    Add: 기본 설정 완료

---
+++ craco.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+	webpack: {
+		alias: {
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@hooks': path.resolve(__dirname, 'src/hooks'),
+		},
+	},
+};
