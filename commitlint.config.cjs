d5fd555731b2882e8c3d3bbd8b611a3feb03f5d6
태관 <tkyoun0421@naver.com>
2024-03-14 17:27:43 +0900

    test

---
+++ commitlint.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "Docs",
+        "Feat",
+        "Design",
+        "Fix",
+        "Refactor",
+        "Move",
+        "Rename",
+        "Remove",
+        "Chore",
+        "Add",
+        "HOTFIX",
+        "Test",
+      ],
+    ],
+    "type-case": [2, "always", "pascal-case"],
+  },
+};
