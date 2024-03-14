29c0dff1d2cb19686903ab61f778588a79eacba1
태관 <tkyoun0421@naver.com>
2024-03-14 16:47:51 +0900

    test

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,12 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   webpack: {
     alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
 };
