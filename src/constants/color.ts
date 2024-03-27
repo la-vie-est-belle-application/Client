43ac210d8168770b0f4b8dace4dab08e7ae28e97
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2024-03-27 16:12:27 +0900

    Add: color & font 객체 선언 및 이미지 asset 폴더에 추가 (#7)

---
+++ src/constants/color.ts
@@ -0,0 +1,28 @@
+interface Colors {
+  [key: string]: string;
+}
+
+export const COLORS: Colors = {
+  blue: "#1e90ff",
+  white: "FFFFFF",
+  gray100: "#F9F9FA",
+  gray200: "#F1F5F8",
+  gray300: "#E5E9ED",
+  gray400: "#D0DAE3",
+  gray500: "#B4BCC6",
+  gray600: "#9199A4",
+  gray700: "#3A7BDF",
+  gray800: "#3F445C",
+  gray900: "#000000",
+  purple100: "#F2DEFF",
+  purple200: "#C0ACFF",
+  purple300: "#A28EFF",
+  purple400: "#705CFC",
+  purple500: "#6652F2",
+  purple600: "#5C48E8",
+  purple700: "#523EDE",
+  purple800: "#4834d4",
+  success: "#0CBC72",
+  warning: "#FFE600",
+  error: "#EA2027",
+};
