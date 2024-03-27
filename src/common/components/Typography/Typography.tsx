0590b668221abd1df936f18e4b853e9bd25a7081
태관 <tkyoun0421@naver.com>
2024-03-27 16:59:13 +0900

    Chore: 타이포그래피 타입 분리

--- src/common/components/Typography/Typography.tsx
+++ src/common/components/Typography/Typography.tsx
@@ -1,81 +1,23 @@
 import { COLORS, Colors } from "@constants/color";
-import styled, { CSSProperties, css } from "styled-components";
-
-const fontDesignSystem = {
-  subtitle1: css`
-    font-size: 4rem;
-    line-height: 1.5;
-    font-weight: 700;
-  `,
-  subtitle2: css`
-    font-size: 3.2rem;
-    line-height: 1.5;
-    font-weight: 700;
-  `,
-  subtitle3: css`
-    font-size: 2.8rem;
-    line-height: 1.5;
-    font-weight: 700;
-  `,
-  subtitle4: css`
-    font-size: 2.4rem;
-    line-height: 1.5;
-    font-weight: 700;
-  `,
-  subtitle5: css`
-    font-size: 2rem;
-    line-height: 1.5;
-    font-weight: 700;
-  `,
-  subtitle6: css`
-    font-size: 1.6rem;
-    line-height: 1.5;
-    font-weight: 700;
-  `,
-  body1: css`
-    font-size: 1.8rem;
-    line-height: 1.5;
-  `,
-  body2: css`
-    font-size: 1.6rem;
-    line-height: 1.5;
-  `,
-  body3: css`
-    font-size: 1.4rem;
-    line-height: 1.5;
-  `,
-  body4: css`
-    font-size: 1.2rem;
-    line-height: 1.5;
-    font-weight: 700;
-  `,
-  caption1: css`
-    font-size: 1.2rem;
-    line-height: 1.5;
-  `,
-  caption2: css`
-    font-size: 1rem;
-    line-height: 1.5;
-  `,
-};
-type FontDesignSystem = keyof typeof fontDesignSystem;
+import { FONT_DESIGN_SYSTEM, FontDesignSystem } from "@constants/typography";
+import styled, { CSSProperties } from "styled-components";
 
 interface TypographyProps {
   fontType?: FontDesignSystem;
   color?: Colors;
   textAlign?: CSSProperties["textAlign"];
-  fontWeight?: CSSProperties["fontWeight"];
+  bold?: boolean;
   cursor?: CSSProperties["cursor"];
 }
 
 const Typography = styled.span<TypographyProps>(
-  ({ color = COLORS.black900, textAlign = "center", fontWeight, cursor }) => ({
+  ({ color = COLORS.black900, textAlign = "center", bold, cursor }) => ({
     color: COLORS[color as keyof typeof COLORS],
     textAlign,
-    fontWeight: fontWeight && fontWeight,
+    fontWeight: bold ? 700 : 400,
     cursor,
   }),
-  ({ fontType = "body3" }) => fontDesignSystem[fontType],
+  ({ fontType = "body4" }) => FONT_DESIGN_SYSTEM[fontType],
 );
 
 export default Typography;
