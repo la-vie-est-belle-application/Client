0e1b2bdaddf545697c25ad4d0f56c4c48d120041
태관 <tkyoun0421@naver.com>
2024-03-28 17:39:12 +0900

    Feat: typography storybook 등록 완료

---
+++ src/stories/Typography/Typography.tsx
@@ -0,0 +1,23 @@
+import { Colors } from "@constants/color";
+import { FONT_DESIGN_SYSTEM, FontDesignSystem } from "@constants/typography";
+import styled, { CSSProperties } from "styled-components";
+
+interface TypographyProps {
+  type?: FontDesignSystem;
+  color?: Colors;
+  $textAlign?: CSSProperties["textAlign"];
+  $bold?: boolean;
+  $cursor?: CSSProperties["cursor"];
+}
+
+const Typography = styled.span<TypographyProps>(
+  ({ color = "gray900", $textAlign = "center", $bold = false, $cursor }) => ({
+    color,
+    $textAlign,
+    fontWeight: $bold ? 700 : 400,
+    $cursor,
+  }),
+  ({ type = "body4" }) => FONT_DESIGN_SYSTEM[type],
+);
+
+export default Typography;
