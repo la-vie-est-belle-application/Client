e1fc9e18804a9a5baeb7daab12e792bfc27ee252
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2024-04-25 15:45:09 +0900

    Refactor: 버튼 스토리 리팩토링 및 이미지파일 삭제 (#14)
    Refactor: button story 리팩토링 및 이미지 파일 삭제
    
    button story 리팩토링
    이미지 파일 삭제

--- src/common/components/Button/buttonStyle.ts
+++ src/common/components/Button/buttonStyle.ts
@@ -2,50 +2,74 @@ import { COLORS } from "@constants/color";
 import { css } from "styled-components";
 
 export type ButtonDesignSystem = keyof typeof BUTTON_DESIGN_SYSTEM;
+export type ButtonSizeSystem = keyof typeof BUTTON_SIZE_SYSTEM;
 
 export const BUTTON_DESIGN_SYSTEM = {
   signIn: css`
     border-radius: 10px;
     background-color: ${COLORS.purple400};
-    width: 20.938rem;
+    width: 100%;
     height: 3.25rem;
     color: ${COLORS.white};
     box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
   `,
   large: css`
     width: 10rem;
-    height: 2.813rem;
+    height: 3rem;
     border-radius: 10px;
     color: ${COLORS.purple600};
     background-color: ${COLORS.purple200};
   `,
   headerIcon: css`
     background-color: ${COLORS.purple400};
-    width: 1.5rem;
-    height: 1.5rem;
+    width: 3rem;
+    height: 3rem;
   `,
   icon: css`
     background-color: ${COLORS.white};
-    width: 1.5rem;
-    height: 1.5rem;
+    width: 3rem;
+    height: 3rem;
   `,
   toggle: css`
     border-radius: 50%;
     background-color: ${COLORS.purple400};
-    width: 2.5rem;
-    height: 2.5rem;
+    width: 3.5rem;
+    height: 3.5rem;
     box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
   `,
   scheduleCancle: css`
     border-radius: 50%;
     background-color: ${COLORS.white};
-    width: 2.5rem;
-    height: 2.5rem;
+    width: 3.5rem;
+    height: 3.5rem;
     box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
   `,
   signOut: css`
     background-color: ${COLORS.white};
-    width: 6.5rem;
+    width: 8rem;
     height: 1.5rem;
+  `,
+};
+
+export const BUTTON_SIZE_SYSTEM = {
+  midium: css`
+    width: 10rem;
+    height: 2.5rem;
+  `,
+  large: css`
+    width: 13rem;
+    height: 3.5rem;
+  `,
+  toggleSmall: css`
+    width: 3rem;
+    height: 3rem;
+  `,
+  toggleMidium: css`
+    width: 3.5rem;
+    height: 3.5rem;
+  `,
+  toggleLarge: css`
+    width: 4.5rem;
+    height: 4.5rem;
   `,
 };
