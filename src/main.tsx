d239cae99b07d4f93c7bb9a634d54866b2e5cef2
junyeoplee <jyeop920@gmail.com>
2024-05-02 14:16:25 +0900

    Feat: modal story 구현
    Modal Story 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./main.css";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const queryClient: QueryClient = new QueryClient({
   defaultOptions: {
@@ -13,9 +15,13 @@ const queryClient: QueryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
+    <ChakraProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
 );
