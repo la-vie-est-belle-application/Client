333653d063c90b9b7303ec572a1535e346ac911c
태관 <tkyoun0421@naver.com>
2024-05-08 15:43:31 +0900

    Feat: 로그인 화면 퍼블리싱 완료

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,76 @@
+import ButtonItem from "@components/Button/Button";
+import Typography from "@components/Typography/Typography";
+import styled from "styled-components";
+
+const SignIn = () => {
+  return (
+    <SignInWrap>
+      <SignInBackgroundImg />
+      <SignInContainer>
+        <SignInTexts>
+          <h1>
+            <Typography type="body2" color="white">
+              라비에벨 진행팀 애플리케이션
+            </Typography>
+          </h1>
+          <SignInSubText>
+            <Typography type="subtitle6" color="white">
+              내 일정에 맞게 원하는 날에 스케줄을 신청하고 급여를 계산할 수
+              있습니다.
+            </Typography>
+          </SignInSubText>
+        </SignInTexts>
+        <ButtonItem type="signIn">
+          <Typography>카카오 로그인</Typography>
+        </ButtonItem>
+      </SignInContainer>
+    </SignInWrap>
+  );
+};
+
+export default SignIn;
+
+const SignInWrap = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
+
+const SignInContainer = styled.div`
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+`;
+
+const SignInBackgroundImg = styled.div`
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  background: url("./public/assets/main.svg") no-repeat center center/cover;
+  z-index: -2;
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
+    z-index: -1;
+  }
+`;
+
+const SignInTexts = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.4rem;
+`;
+
+const SignInSubText = styled.p`
+  width: 17.8rem;
+  word-break: keep-all;
+`;
