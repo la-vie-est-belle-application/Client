54b0cfb2deb945ad8e4af8e906951e07a65d0d3d
태관 <tkyoun0421@naver.com>
2024-05-08 14:13:59 +0900

    Feat: 스케쥴 디테일 링크 컴포넌트 완성

--- src/common/components/ScheduleDetailLink/ScheduleDetailLink.tsx
+++ src/common/components/ScheduleDetailLink/ScheduleDetailLink.tsx
@@ -5,47 +5,76 @@ import styled from "styled-components";
 
 interface ScheduleDetailLinkProps {
   scheduleDate: string;
-  $isScheduleStatus: boolean;
-  $isDone: boolean;
+  $isCanceled: boolean;
+  $isFixed: boolean;
   to?: string;
 }
 
 interface ScheduleStatusProps {
-  $isScheduleStatus: boolean;
+  $isFixed: boolean;
 }
 
-const ScheduleDetailLink = ({ ...props }: ScheduleDetailLinkProps) => {
-  const { scheduleDate, $isScheduleStatus, to } = props;
-
+const ScheduleDetailLink = ({
+  scheduleDate,
+  $isCanceled,
+  $isFixed,
+  to,
+}: ScheduleDetailLinkProps) => {
   return (
-    <ScheduleDetailLinkWrap>
-      <Link to={to || "/"}>
+    <ScheduleDetailLinkWrap $isCanceled={$isCanceled}>
+      <ScheduleLink to={to || "/"}>
         <Typography type="subtitle6" color={"white"}>
           {scheduleDate}
         </Typography>
-        <ScheduleStatus $isScheduleStatus={$isScheduleStatus} />
-      </Link>
+        <ScheduleStatus $isFixed={$isFixed} />
+      </ScheduleLink>
     </ScheduleDetailLinkWrap>
   );
 };
 
 export default ScheduleDetailLink;
 
-const ScheduleDetailLinkWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1.8rem 1.2rem;
+const ScheduleDetailLinkWrap = styled.div<
+  Pick<ScheduleDetailLinkProps, "$isCanceled">
+>`
+  position: relative;
+  width: 100%;
   border-radius: 0.8rem;
   background-color: ${COLORS.purple300};
-  width: 100%;
+  ${(props) =>
+    props.$isCanceled &&
+    `
+    &::after {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: ${COLORS.gray900};
+      opacity: 0.4;
+      border-radius: 0.8rem;
+    }
+  `}
 `;
 
 const ScheduleStatus = styled.div<ScheduleStatusProps>`
+  position: absolute;
+  top: 50%;
+  right: 1.2rem;
+  transform: translateY(-50%);
   width: 1rem;
   height: 1rem;
   border-radius: 50%;
   background-color: ${(props) =>
-    props.$isScheduleStatus ? COLORS.success : COLORS.gray100};
-  margin-left: auto;
+    props.$isFixed ? COLORS.success : COLORS.gray100};
+`;
+
+const ScheduleLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1.8rem 1.2rem;
+  width: 100%;
+  height: 100%;
 `;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,5 @@
-import ButtonItem from "@components/Button/Button";
-import ScheduleDetailLink from "@components/ScheduleDetailLink/ScheduleDetailLink";
-
 const Home = () => {
-  return (
-    <div>
-      <ScheduleDetailLink
-        scheduleDate="20203"
-        $isScheduleStatus={false}
-        $isDone={false}
-      ></ScheduleDetailLink>
-      <ButtonItem type="signIn">asdasdasd</ButtonItem>
-    </div>
-  );
+  return <></>;
 };
 
 export default Home;
