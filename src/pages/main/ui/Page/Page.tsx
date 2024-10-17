b3da3fc51e82b150aaa836a3aafb67ec167c67fa
태관 <tkyoun0421@naver.com>
2024-10-17 15:31:48 +0900

    Chore: eslint cnrk

--- src/pages/main/ui/Page/Page.tsx
+++ src/pages/main/ui/Page/Page.tsx
@@ -1,12 +1,10 @@
 import { Button } from "@chakra-ui/react";
-import { useAuth } from "@entities/login/hooks/useAuth";
 
 export const Page = () => {
-  const { user, handleLogout } = useAuth();
   return (
     <>
-      <div>{user?.name}</div>
-      <Button onClick={handleLogout}>로그아웃</Button>
+      <div>이름</div>
+      <Button>로그아웃</Button>
     </>
   );
 };
