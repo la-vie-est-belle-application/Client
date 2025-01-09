061486a558ebb2744a05e9a663ae7ed0325bdbad
Taegwan Youn <tkyoun0421@naver.com>
2025-01-09 21:40:55 +0900

    Chore: kakaoAuth 제거

--- src/pages/login/ui/Page/Page.tsx
+++ src/pages/login/ui/Page/Page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@chakra-ui/react";
-import { useKakaoAuth } from "@entities/login/hooks/useKakaoAuth";
 
 export const LoginPage = () => {
-  const { handleKakaoLogin } = useKakaoAuth();
   return (
     <>
-      <Button onClick={handleKakaoLogin}>카카오로 로그인하기</Button>
+      <Button>카카오로 로그인하기</Button>
     </>
   );
 };
