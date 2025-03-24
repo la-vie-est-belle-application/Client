ed14a226768828124fc81f6bcd4d4678031be877
Taegwan Youn <tkyoun0421@naver.com>
2025-03-25 01:04:32 +0900

    feat: husky 설치

---
+++ commitlint.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  parserPreset: "conventional-changelog-conventionalcommits",
+  rules: {
+    "body-leading-blank": [1, "always"],
+    "body-max-line-length": [2, "always", 100],
+    "footer-leading-blank": [1, "always"],
+    "footer-max-line-length": [2, "always", 100],
+    "header-max-length": [2, "always", 100],
+    "subject-case": [2, "never", "lower-case"],
+  },
+};
