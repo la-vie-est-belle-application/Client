90b1f06b6b8119b261a6daa0b306aca2bbedf879
junyeoplee <jyeop920@gmail.com>
2025-04-01 18:03:57 +0900

    feat: 사이드바 메뉴리스트 퍼블리싱 .git/COMMIT_EDITMSG  #69

--- src/widgets/side-bar/side-bar.tsx
+++ src/widgets/side-bar/side-bar.tsx
@@ -3,14 +3,44 @@
 import {
   Sidebar,
   SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
   SidebarProvider,
 } from "@/components/ui/sidebar";
+import { SideBarItem } from "./type";
 
 export const SideBar = () => {
+  const sideBarItems: SideBarItem[] = [
+    {
+      title: "스케줄 등록",
+      url: "/schedule",
+    },
+    {
+      title: "스케줄 관리",
+      url: "#",
+    },
+  ];
   return (
     <SidebarProvider>
-      <Sidebar className="z-1">
-        <SidebarContent>SideBar</SidebarContent>
+      <Sidebar className="z-1 pt-20">
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {sideBarItems.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <span className="text-sm font-bold">{item.title}</span>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
       </Sidebar>
     </SidebarProvider>
   );
