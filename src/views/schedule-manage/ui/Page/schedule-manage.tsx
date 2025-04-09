f7f2b8a072d84541292b45647b1d7c522dcc3a09
junyeoplee <jyeop920@gmail.com>
2025-04-09 16:03:01 +0900

    feat: 셀렉트 , 조회 버튼 퍼블리싱 .git/COMMIT_EDITMSG  #73

--- src/views/schedule-manage/ui/Page/schedule-manage.tsx
+++ src/views/schedule-manage/ui/Page/schedule-manage.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { FaMagnifyingGlass, FaRegCalendar } from "react-icons/fa6";
+import { Button } from "@/components/ui/index";
+import { SelectOption } from "@/src/feature/schedule-manage/index";
 
 export const ScheduleManagePage = () => {
   return (
     <section className="w-full h-full p-6">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold mb-4">스케줄 관리</h1>
+      <h1 className="text-2xl font-bold mb-4">스케줄 관리</h1>
+      <div className="flex justify-start items-center gap-4 max-w-[1000px]">
+        <SelectOption className="w-full" icon={<FaRegCalendar />} />
+        <SelectOption className="w-full" icon={<FaRegCalendar />} />
+
+        <Button variant="primary">
+          <FaMagnifyingGlass />
+          조회
+        </Button>
       </div>
     </section>
   );
