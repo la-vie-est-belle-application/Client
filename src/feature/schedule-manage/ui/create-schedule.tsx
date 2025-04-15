6b71300a56a7a55279398d5937b88c233de3ac6b
junyeoplee <jyeop920@gmail.com>
2025-04-15 16:52:48 +0900

    feat: 스케줄 등록버튼 dialog로 변경 .git/COMMIT_EDITMSG  #79

--- src/feature/schedule-manage/ui/create-schedule.tsx
+++ src/feature/schedule-manage/ui/create-schedule.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { FaPlus } from "react-icons/fa";
-import { Button } from "@/components/ui/index";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/index";
 
 export const CreateSchedule = () => {
   return (
-    <Button variant="primary">
-      <FaPlus />
-      스케줄 등록
-    </Button>
+    <Dialog>
+      <DialogTrigger>
+        <Button variant="primary">
+          <FaPlus />
+          스케줄 등록
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-xl">
+            스케줄 등록 하시겠습니까?
+          </DialogTitle>
+        </DialogHeader>
+
+        <div className="flex gap-2 pt-15">
+          <Button variant="ghost" className="flex-1" size="lg">
+            취소
+          </Button>
+          <Button variant="primary" className="flex-1" size="lg">
+            등록
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
