449de66431cb33c6e2c62db5a73efe4bbbfcb7a9
Taegwan Youn <tkyoun0421@naver.com>
2025-04-16 00:31:18 +0900

    chore: supabase 오류 수정 .git/COMMIT_EDITMSG  #68

--- src/entities/supabase/index.ts
+++ src/entities/supabase/index.ts
@@ -1,4 +1,4 @@
 export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-export const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+export const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ARON_KEY!;
 
 export { default as supabase } from "./config/server";
