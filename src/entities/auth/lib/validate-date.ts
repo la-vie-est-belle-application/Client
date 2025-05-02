c9281b4cfff5a693ef67535586b79482bc7dfb99
Taegwan Youn <tkyoun0421@naver.com>
2025-05-02 16:59:05 +0900

    docs: fsd 폴더 구조 문서 생성 .git/COMMIT_EDITMSG  #89

--- src/entities/auth/lib/validate-date.ts
+++ src/entities/auth/lib/validate-date.ts
@@ -1,4 +1,4 @@
-interface ParsedDate  {
+interface ParsedDate {
   year: number;
   month: number;
   day: number;
@@ -6,23 +6,21 @@ interface ParsedDate  {
 
 export const isValidDate = (dateStr: string): boolean => {
   const isEightDigits = (dateStr: string) => /^\d{8}$/.test(dateStr);
-  
-  const splitDateString = (dateStr: string): ParsedDate  => ({
+
+  const splitDateString = (dateStr: string): ParsedDate => ({
     year: parseInt(dateStr.slice(0, 4)),
     month: parseInt(dateStr.slice(4, 6)),
     day: parseInt(dateStr.slice(6, 8)),
   });
 
-  const isRealDate = ({year, month, day}: ParsedDate ): boolean => {
+  const isRealDate = ({ year, month, day }: ParsedDate): boolean => {
     const date = new Date(year, month - 1, day);
     return (
       date.getFullYear() === year &&
       date.getMonth() === month - 1 &&
       date.getDate() === day
-    )
-  }
+    );
+  };
 
-  return isEightDigits(dateStr) && isRealDate(splitDateString(dateStr))
-
+  return isEightDigits(dateStr) && isRealDate(splitDateString(dateStr));
 };
-
